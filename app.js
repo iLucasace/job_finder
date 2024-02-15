d582b18043341df0d63b240a92bf1ae9807dd228
iLucasace <lucas.ace@hotmail.com>
2024-02-15 17:38:48 -0300

    routes added

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 app.listen(PORT, function() {
     console.log("Port:", PORT);
 });
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 //DB Connection
 db 
@@ -17,7 +21,5 @@ db
         console.log("An error occurred connecting to the database!")
     });
 
-//Routes
-app.get('/', (req, res) => {
-    res.send("Working!");
-});
+//Job Routes
+app.use('/jobs', require('./routes/jobs'));
